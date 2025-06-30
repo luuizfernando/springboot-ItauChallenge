e66621107a71e8c73fa889558f4e8d73a929f6be
Luiz Fernando <luizfernandosant26@gmail.com>
2025-06-30 16:45:38 -0300

    Update TransactionRepository.java

--- src/main/java/com/projects/itau_challenge/repositories/TransactionRepository.java
+++ src/main/java/com/projects/itau_challenge/repositories/TransactionRepository.java
@@ -1,9 +1,11 @@
 package com.projects.itau_challenge.repositories;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projects.itau_challenge.domain.transaction.Transaction;
 
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
    
 }
